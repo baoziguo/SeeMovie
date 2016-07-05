5ed3e238cb601729ae9562a6ea71ecda2c966bc7
276938427@qq.com <276938427@qq.com>
2016-07-05 15:13:01 +0800

    初始化项目by:baozi

---
+++ app/src/main/java/com/baozi/movie/adapter/ContactAdapter.java
@@ -0,0 +1,48 @@
+package com.baozi.movie.adapter;
+
+import android.content.Context;
+import android.view.View;
+
+import java.util.Collection;
+
+import com.baozi.seemovie.R;
+import com.baozi.movie.adapter.base.BaseRecyclerAdapter;
+import com.baozi.movie.adapter.base.BaseRecyclerHolder;
+import com.baozi.movie.adapter.base.IMutlipleItem;
+import com.baozi.movie.bean.Friend;
+import com.baozi.movie.bean.User;
+import com.baozi.movie.db.NewFriendManager;
+
+/**联系人
+ * 一种简洁的Adapter实现方式，可用于多种Item布局的recycleView实现，不用再写ViewHolder啦
+ * @author :smile
+ * @project:ContactNewAdapter
+ * @date :2016-04-27-14:18
+ */
+public class ContactAdapter extends BaseRecyclerAdapter<Friend> {
+
+    public static final int TYPE_NEW_FRIEND = 0;
+    public static final int TYPE_ITEM = 1;
+
+    public ContactAdapter(Context context, IMutlipleItem<Friend> items, Collection<Friend> datas) {
+        super(context,items,datas);
+    }
+
+    @Override
+    public void bindView(BaseRecyclerHolder holder, Friend friend, int position) {
+        if(holder.layoutId==R.layout.item_contact){
+            User user =friend.getFriendUser();
+            //好友头像
+            holder.setImageView(user == null ? null : user.getAvatar(), R.mipmap.head, R.id.iv_recent_avatar);
+            //好友名称
+            holder.setText(R.id.tv_recent_name,user==null?"未知":user.getUsername());
+        }else if(holder.layoutId==R.layout.header_new_friend){
+            if(NewFriendManager.getInstance(context).hasNewFriendInvitation()){
+                holder.setVisible(R.id.iv_msg_tips,View.VISIBLE);
+            }else{
+                holder.setVisible(R.id.iv_msg_tips, View.GONE);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/baozi/movie/ui/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.baozi.movie.ui;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.baozi.seemovie.R;
+import com.baozi.movie.bean.User;
+import com.baozi.movie.base.BaseActivity;
+import com.baozi.movie.model.UserModel;
+
+/**启动界面
+ * @author :smile
+ * @project:SplashActivity
+ * @date :2016-01-15-18:23
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler =new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                User user = UserModel.getInstance().getCurrentUser();
+                if (user == null) {
+                    startActivity(LoginActivity.class,null,true);
+                }else{
+                    startActivity(MainActivity.class,null,true);
+                }
+            }
+        },1000);
+
+    }
+}
